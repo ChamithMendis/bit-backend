3dec816b60cce170eb2af690e4bb566b41846040
ChamithMendis <chamith109mendes@gmail.com>
2024-11-02 20:02:03 +0530

    authentications changes

--- src/main/java/com/bit/backend/repositories/UserRepository.java
+++ src/main/java/com/bit/backend/repositories/UserRepository.java
@@ -16,7 +16,11 @@ public interface UserRepository extends JpaRepository<User, Long> {
 
     Optional<User> findByLogin(String login);
 
-    @Query(nativeQuery = true, value = "SELECT * FROM get_auth_ids WHERE user_id = :userId")
+    @Query(nativeQuery = true, value = "select auth_details.auth_id as auth_id, :userId as user_id from " +
+                                       "(select * from auth_group_users auth_users where auth_users.user_Id = :userId) auth_users\n" +
+                                       "join (select * from ems.auth_group_authentication auth_details) as auth_details\n" +
+                                       "on auth_users.auth_group_id = auth_details.auth_group_id\n" +
+                                       "group by auth_details.auth_id")
     Optional<List<Integer>> findAuthIdsByUserId(@Param("userId") long userId);
 
 
